df6108c824993d27daf39fb6c64233485be41f28
shufo <meikyowise@gmail.com>
2017-07-27 00:22:21 +0900

    Update probot.js

--- .probot.js
+++ .probot.js
@@ -1,19 +1,7 @@
-// Delete :+1: comments
-const singleEmoji = /^\W*(:[\w-+]+:|[\uE000-\uF8FF]|\uD83C[\uDF00-\uDFFF]|\uD83D[\uDC00-\uDDFF])\W*$/g;
-on('issue_comment.created')
-  .filter(event => event.payload.comment.body.match(singleEmoji))
-  .deleteComment();
+ on('issues.opened').comment(`
+        Hello @{{ sender.login }}. Thanks for inviting me to your project.
+        Read more about [all the things I can help you with][config]. I can't
+        wait to get started!
 
-function isDeleted(event) {
-  return event.payload.action === 'deleted' &&
-  event.payload.sender.type !== 'Bot';
-}
-
-// restore deleted comments but the one deleted by PRobot
-on('issue_comment', 'commit_comment', 'pull_request_review_comment')
-.filter(isDeleted)
-.comment(`
-Deleted comment from @{{ comment.user.login }} at {{ comment.updated_at }}
----
-{{ comment.body }}
+        [config]: https://github.com/bkeepers/PRobot/blob/master/docs/configuration.md
 `);
