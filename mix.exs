cdd1ee3c28b381a8e30aef96b42b152e8ab1eb11
shufo <meikyowise@gmail.com>
2018-02-05 22:01:08 +0900

    Bump paidy-elixir to 0.2.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Paidy.Mixfile do
   def project do
     [
       app: :paidy,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.4",
       description: description(),
       build_embedded: Mix.env() == :prod,
